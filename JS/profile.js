20ddd7c209f5aab8e99de593651bfff78da3cbec
david ibrahim <david@MacBook-Pro.local>
2024-11-13 12:36:01 -0500

    fix first click 3

--- JS/profile.js
+++ JS/profile.js
@@ -1,16 +1,21 @@
-
 const images = document.querySelectorAll('.image-container');
 
   images.forEach((imageContainer) => {
     let isPressed = false;
     const image = imageContainer.querySelector('.image');
     const link = imageContainer.querySelector('a');
+    const isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
 
     image.addEventListener('click', function(event) {
-      if (!isPressed) {
-        image.classList.add('hover-effect');
-        isPressed = true;
-        event.preventDefault();  
+      if (isMobile) {
+        if (!isPressed) {
+          image.classList.add('hover-effect');
+          isPressed = true;
+          event.preventDefault(); 
+        } else {
+          window.location.href = link.href;
+          isPressed= false;
+        }
       } else {
         window.location.href = link.href;
       }
